b17cb4bb3bb092d0d0da3fe1029cf22b9429360b
pabloramoss <pablo-362@hotmail.com>
2023-11-05 18:26:51 +0100

    feat: add possibility to add more steps in stepper

--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -6,10 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./stepper.component.css']
 })
 export class StepperComponent {
+  totalSteps = 6;
+  stepsArray = Array(this.totalSteps).fill(0).map((x, i) => i + 1);
   public currentStep = 1;
+  
 
   nextStep() {
-    if (this.currentStep < 4) {
+    if (this.currentStep < this.totalSteps) {
       this.currentStep++;
     }
   }
